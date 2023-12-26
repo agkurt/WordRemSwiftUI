a658131caee0372f3b2dd922a6aaf3de83d7818a
Ahmet Göktürk Kurt <ahmetgokturkkurt3@gmail.com>
2023-12-26 20:00:31 +0300

    Initial Commit

---
+++ WordRemSwiftUI/WordRemSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordRemSwiftUIApp.swift
+//  WordRemSwiftUI
+//
+//  Created by Ahmet Göktürk Kurt on 26.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct WordRemSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
